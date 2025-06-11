33240e147c9d02bb571ce2e6023ebbd9554d2620
nugongja <nugongja@gmail.com>
2025-06-11 12:07:48 +0900

    Delete spring_server directory

--- spring_server/src/main/java/capston/capston_spring/dto/fakeFeedbackResponse.java
+++
@@ -1,7 +0,0 @@
-package capston.capston_spring.dto;
-
-public class fakeFeedbackResponse {
-    public String feedback;
-    public String userImageURL;
-    public String expertImageURL;
-}
--- spring_server/src/main/java/capston/capston_spring/exception/SessionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package capston.capston_spring.exception;
-
-public class SessionNotFoundException extends RuntimeException {
-    public SessionNotFoundException(String message) {
-        super(message);
-    }
-}
